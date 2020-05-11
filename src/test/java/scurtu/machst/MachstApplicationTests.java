e9b654028cbb355cf657f8c05e9181d0d26574e3
Scurtu Andrei <web.scurtu@gmail.com>
2020-05-11 21:49:26 +0200

    Test2

---
+++ src/test/java/scurtu/machst/MachstApplicationTests.java
@@ -0,0 +1,13 @@
+package scurtu.machst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
